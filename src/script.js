a8f89da70da0eaca4c1d68a1456a4f2055476d53
mr. whalen <michael.whalen@sitechhs.com>
2021-06-07 07:48:33 -0400

    Ready

---
+++ src/script.js
@@ -0,0 +1,27 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+
+/**
+ * Objects
+ */
+
+///scale
+
+//Rotation
+
+/**
+ * Sizes
+ */
+
+/**
+ * Camera
+ */
+
+/**
+ * Renderer
+ */

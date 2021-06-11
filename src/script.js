5c6fbbd4f7aac469a6f8cf59a554e57369e1f8e8
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-06-11 09:00:01 -0400

    attempt

--- src/script.js
+++ src/script.js
@@ -5,11 +5,17 @@ import * as THREE from "three";
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-
+const scene = new THREE.Scene(); //instantiation or new instance
 /**
  * Objects
  */
-
+const geometry = new THREE.BoxGeometry(1, 1, 1,3,3,3); // 6 sides but its a cube
+const material = new THREE.MeshBasicMaterial({ color: 0x00f0ff });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 1
+mesh.position.y = 1
+mesh.position.z = 1
+scene.add(mesh);
 ///scale
 
 //Rotation
@@ -17,11 +23,21 @@ const canvas = document.querySelector("canvas.webgl");
 /**
  * Sizes
  */
+const sizes = {
+width: 800, 
+height: 600,
+}
 
 /**
  * Camera
  */
-
+const camera = new.THREE.PerspectiveCamera(75, sizes.width/sizes.height )
+camera.position.z = 7
+camera.position.y = 2
+camera.position.x = 1
+scene.add(scene, camera)
 /**
  * Renderer
  */
+const renderer = new THREE.WebGL1Renderer({canvas: canvas, })
+renderer
